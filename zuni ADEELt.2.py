50a66b2eb5f264cadd9d0417c8d8a5e17e1cb621
zunairaAdeel <156577719+zunairaAdeel@users.noreply.github.com>
2024-01-14 15:31:04 +0500

    Add files via upload

---
+++ zuni ADEELt.2.py
@@ -0,0 +1,11 @@
+num1=int(input("enter your first number"))
+num2=int(input("enter your second number"))
+operator=input("enter your operator(+,-,/,*):")
+if operator=="+":
+ print(num1+num2)
+elif operator=="-":
+ print(num1-num2)
+elif operator=="/":
+ print(num1/num2)
+elif operator=="*":
+    print(num1*num2)
---
+++ zuni.2.output.py
@@ -0,0 +1,17 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======= RESTART: //192.168.14.248/profile/gdu22/Desktop/zuni.inpiut.2.py =======
+enter your first number5
+enter your second number5
+enter your operator(+,-,/,*):+
+Traceback (most recent call last):
+  File "//192.168.14.248/profile/gdu22/Desktop/zuni.inpiut.2.py", line 5, in <module>
+    result=add(num1,num2)
+NameError: name 'add' is not defined
+>>> 
+======= RESTART: //192.168.14.248/profile/gdu22/Desktop/zuni.inpiut.2.py =======
+enter your first number5
+enter your second number5
+enter your operator(+,-,/,*):+
+5 5
