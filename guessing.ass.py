d1b023c583e26c7fd4dd3f859303e2f20fe73617
zunairaAdeel <156577719+zunairaAdeel@users.noreply.github.com>
2024-02-04 17:10:23 +0500

    Add files via upload

---
+++ guessing.ass.py
@@ -0,0 +1,12 @@
+import random
+    
+num = random.randint(1, 10)
+guess = None    
+while guess != num:
+    guess = input("guess a number between 1 and 10: ")
+    guess = int(guess) 
+    if guess == num:
+        print("congratulations! you won!")
+        break
+    else:
+        print("sorry. try again!")
